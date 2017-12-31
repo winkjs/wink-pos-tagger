b56be1a93276eb0cdff703cce45fb53d10994f08
Sanjaya <sanjaya@graype.in>
2017-12-31 22:39:18 +0530

    chore(*): add license header

--- test/lemmatize-vbx-specs.js
+++ test/lemmatize-vbx-specs.js
@@ -1,3 +1,26 @@
+//     wink-pos-tagger
+//     Optimal English Part-of-speech (POS) tagger
+//
+//     Copyright (C) 2017  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-pos-tagger”.
+//
+//     “wink-pos-tagger” is free software: you can redistribute
+//     it and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-pos-tagger” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-pos-tagger”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+//
 /* eslint-disable no-console */
 
 var chai = require( 'chai' );
